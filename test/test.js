a4cb802184ae02fefb8a61824307f928e0f108cd
Christian Talmo <talmo.christian@gmail.com>
2020-02-03 07:49:39 +0200

    test: max listener limit warnings test: throw error when callback is not a function

--- test/test.js
+++ test/test.js
@@ -72,3 +72,33 @@ test( 'duplicate callback error', function ( t ) {
 
   t.ok( _error )
 } )
+
+test( 'non function callback', function ( t ) {
+  t.plan( 2 )
+  const ee = eeh()
+
+  const callback = {}
+  try {
+    ee.on( 'msg', callback )
+  } catch ( err ) {
+    console.log( err.message )
+    t.ok( err )
+    t.equal( err.message, 'eeto: callback must be a function (was object)' )
+  }
+} )
+
+test( 'warn when exceeding default max listeners limit', function ( t ) {
+  t.plan( 1 )
+  const ee = eeh()
+
+  const _consoleLog = console.log
+  console.log = function ( text ) {
+    console.log = _consoleLog
+    console.log( text )
+    t.equal( text, `eeto max event listener limit warning: Possible event emitter leak detected. 11 'giraffe' listeners added. Set ee.opts.limit to increase limit.` )
+  }
+
+  for ( let i = 0; i < 12; i++ ) {
+    ee.on( 'giraffe', function () {} )
+  }
+} )
